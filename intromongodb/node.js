7d8578df4a78b7afd3915fc5461eb9b7a73527a0
Safa <safae.khess@gmail.com>
2023-08-08 09:35:14 +0200

    ..

--- intromongodb/node.js
+++ intromongodb/node.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const uri = '<mongodb+srv://safaekhess:<password>@cluster0.xucvubn.mongodb.net/>';
+const uri = '<mongodb+srv://safaekhess:<Gracias12>@cluster0.xucvubn.mongodb.net/>';
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 client.connect(err => {
@@ -10,4 +10,19 @@ client.connect(err => {
   }
 
   console.log('Connected to the database!');
+});
+
+const collection = client.db('test').collection('students');
+
+collection.insertOne({
+  name: 'John Doe',
+  age: 25,
+  grade: 'A'
+}, (err, result) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(`Inserted ${result.insertedCount} documents into the collection`);
 });
