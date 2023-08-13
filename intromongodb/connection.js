b0dc5d330367f10700ff1363172f80d36f319485
Safa <safae.khess@gmail.com>
2023-08-13 14:25:40 +0200

    connected to mangodb

---
+++ intromongodb/connection.js
@@ -0,0 +1,16 @@
+const { MongoClient}  = require("mongodb");
+
+const client = new MongoClient("mongodb://localhost:27017", {
+   useNewUrlParser:true
+});
+
+// client.connect((error,dbClient) =>{
+//     if(error){
+//         console.log("ERROR",error);
+//     }
+//     if(dbClient){
+//         console.log("Connected to Mongodb");
+//     }
+// });
+
+client.connect().then(dbClient=>console.log("Connected to Mango db")).catch((error) => console.log("Error,error"));
--- intromongodb/node.js
+++ intromongodb/node.js
@@ -1,38 +1,14 @@
-const MongoClient = require('mongodb').MongoClient;
 
-const uri = '<mongodb+srv://safaekhess:<Gracias12>@cluster0.xucvubn.mongodb.net/>';
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://safaekhess:<password>@cluster0.xucvubn.mongodb.net/?retryWrites=true&w=majority";
+
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
-client.connect(err => {
-  if (err) {
-    console.error(err);
-    return;
+  try {
+    client.connect()
+    console.log('Connected to the database!');   
+  } catch (error) {
+    console.log(error);
+    return
   }
 
-  console.log('Connected to the database!');
-});
-
-const collection = client.db('test').collection('students');
-
-collection.insertOne({
-  name: 'John Doe',
-  age: 25,
-  grade: 'A'
-}, (err, result) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  console.log(`Inserted ${result.insertedCount} documents into the collection`);
-});
-
-collection.find({}).toArray((err, docs) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  console.log(`Found ${docs.length} documents in the collection`)
-  console.log(docs);
-});
